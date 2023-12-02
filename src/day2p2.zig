44b4db89b06a11d1d4c0a7ab3855edad19be2278
davidnadeau <dnadeau@etsy.com>
2023-12-02 16:46:48 -0500

    use array splitting

--- src/day2p2.zig
+++ src/day2p2.zig
@@ -16,66 +16,33 @@ pub fn sumPowers(filename: []const u8) !i32 {
     var powerSums: i32 = 0;
 
     while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
-        var gameIdBuffer: [3]u8 = undefined;
-        var idSize: usize = 0;
-        var searchingForGameId = true;
-        var searchingColourCount = false;
-        var searchingColourName = false;
-
-        var colourBuffer: [2]u8 = undefined;
-        var colourCountSize: usize = 0;
-
-        var colourNameBuffer: [5]u8 = undefined;
-        var colourNameSize: usize = 0;
-
         var fewestRed: i32 = 0;
         var fewestBlue: i32 = 0;
         var fewestGreen: i32 = 0;
 
-        for (4..line.len) |i| {
-            if (searchingForGameId) {
-                if (line[i] == ':') {
-                    searchingForGameId = false;
-                    searchingColourCount = true;
-                } else if (!(line[i] == ' ')) {
-                    gameIdBuffer[idSize] = line[i];
-                    idSize = idSize + 1;
-                }
-            } else if (searchingColourCount) {
-                if (line[i] >= '0' and line[i] <= '9') {
-                    colourBuffer[colourCountSize] = line[i];
-                    colourCountSize = colourCountSize + 1;
-                } else if (colourCountSize > 0 and line[i] == ' ') {
-                    searchingColourCount = false;
-                    searchingColourName = true;
-                }
-            } else if (searchingColourName) {
-                if (line[i] >= 'b' and line[i] <= 'u') {
-                    colourNameBuffer[colourNameSize] = line[i];
-                    colourNameSize = colourNameSize + 1;
-                }
-                if (line[i] == ',' or line[i] == ';' or line[i] == '\n' or i == line.len - 1) {
-                    const dieRollSlice = colourBuffer[0..colourCountSize];
-                    const dieRoll: u8 = try std.fmt.parseInt(u8, dieRollSlice, 10);
-                    const colour = colourNameBuffer[0..colourNameSize];
+        var iter = std.mem.split(u8, line, ": ");
+        const gameId = iter.next().?;
+        _ = gameId;
+        const handsIterator = iter.next().?;
 
-                    if (std.mem.eql(u8, colour, "red")) {
-                        fewestRed = @max(fewestRed, dieRoll);
-                    } else if (std.mem.eql(u8, colour, "blue")) {
-                        fewestBlue = @max(fewestBlue, dieRoll);
-                    } else if (std.mem.eql(u8, colour, "green")) {
-                        fewestGreen = @max(fewestGreen, dieRoll);
-                    }
-
-                    colourBuffer = undefined;
-                    colourCountSize = 0;
-                    colourNameBuffer = undefined;
-                    colourNameSize = 0;
-                    searchingColourCount = true;
-                    searchingColourName = false;
+        var hands = std.mem.split(u8, handsIterator, "; ");
+        while (hands.next()) |hand| {
+            var rolls = std.mem.split(u8, hand, ", ");
+            while (rolls.next()) |roll| {
+                var dieValues = std.mem.split(u8, roll, " ");
+                const countSlice = dieValues.next().?;
+                const count: u8 = try std.fmt.parseInt(u8, countSlice, 10);
+                const colour = dieValues.next().?;
+                if (std.mem.eql(u8, colour, "red")) {
+                    fewestRed = @max(fewestRed, count);
+                } else if (std.mem.eql(u8, colour, "blue")) {
+                    fewestBlue = @max(fewestBlue, count);
+                } else if (std.mem.eql(u8, colour, "green")) {
+                    fewestGreen = @max(fewestGreen, count);
                 }
             }
         }
+
         const power = fewestRed * fewestGreen * fewestBlue;
         powerSums = powerSums + power;
     }
